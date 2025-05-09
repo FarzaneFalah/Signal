01f0af160586c17a2841e5b0079a4f636d124705
FarzaneFalah <farzaneh.falahnikjeh@ucalgary.ca>
2025-05-08 22:43:35 -0600

    Setup project with Vite + React + Typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App: React.FC = () =>{
+  return (
+    <>
+      <div>
+        Canada Food Guide
+      </div>
+    </>
+  )
+}
+
+export default App
